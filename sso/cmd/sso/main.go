cb53bed894664390defc8426660e0e6ae3e604ff
barabekrb <emilfedorov2001@gmail.com>
2024-08-16 16:15:08 +0300

    made logger and config reader

---
+++ sso/cmd/sso/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"sso1/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setUpLogger(cfg.Env)
+
+	log.Info("starting application", slog.String("env", cfg.Env))
+}
+
+func setUpLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+
+}
